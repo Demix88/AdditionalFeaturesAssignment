f15e5fcd32bc6f5b07452e87d437ed5355acf427
Demix88 <skrasnauskis@gmail.com>
2022-04-13 12:42:36 +0100

    Add project files.

---
+++ AdditionalFeaturesAssignment/CompanyUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    
+   public class CompanyUser
+    {
+        public CompanyUser(string name) : this(name, "Jlk.05235.22")
+        {
+
+        }
+        public CompanyUser(string name, string pass)
+        {
+            UserName = name;
+            UserPassword = pass;
+        }
+
+        const string SecretQuestion = "What is your mothers maiden name?";
+        string SecretAnswer { get; set; }
+        string Name { get; set; }
+        string UserName { get; set; }
+        string UserPassword { get; set; }
+        
+
+        
+        }
+    }
